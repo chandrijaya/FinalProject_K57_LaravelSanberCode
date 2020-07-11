0c41f272a942665993c07ab6884df306bc49b581
Muhamad Wisnu Alfiansyah <wisnualfiansyah70@gmail.com>
2020-07-11 23:04:10 +0800

    edit footer

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -5,8 +5,8 @@
             <div class="footer-section">
                 {{-- Jangan Dihapus!! --}}
                 <a class="logo" href="#"><img src={{ asset("/Bona/images/logo.png")}} alt="Logo Image"></a>
-                <p class="copyright">Bona @ 2017. All rights reserved.</p>
-                <p class="copyright">Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                <p class="copyright">Kelompok 57 @ 2020. All rights reserved.</p>
+                <p class="copyright">Powered by <a href="https://colorlib.com" target="_blank">SanberCode</a></p>
                 {{-- /Jangan Dihapus!! --}}
                 <ul class="icons">
                     <li><a href="#"><i class="ion-social-facebook-outline"></i></a></li>
@@ -20,23 +20,7 @@
         </div><!-- col-lg-4 col-md-6 -->
 
         <div class="col-lg-4 col-md-6">
-                <div class="footer-section">
-                <h4 class="title"><b>CATAGORIES</b></h4>
-                <ul>
-                    <li><a href="#">BEAUTY</a></li>
-                    <li><a href="#">HEALTH</a></li>
-                    <li><a href="#">MUSIC</a></li>
-                </ul>
-                <ul>
-                    <li><a href="#">SPORT</a></li>
-                    <li><a href="#">DESIGN</a></li>
-                    <li><a href="#">TRAVEL</a></li>
-                </ul>
-            </div><!-- footer-section -->
-        </div><!-- col-lg-4 col-md-6 -->
-
-        <div class="col-lg-4 col-md-6">
-            <div class="footer-section">
+            <!-- <div class="footer-section">
 
                 <h4 class="title"><b>SUBSCRIBE</b></h4>
                 <div class="input-area">
@@ -48,6 +32,35 @@
 
             </div><!-- footer-section -->
         </div><!-- col-lg-4 col-md-6 -->
+
+        <div class="col-lg-4 col-md-6">
+                <div class="footer-section">
+                <h4 class="title"><b>Contributors :</b></h4>
+                <ul>
+                    <li><a href="#">Hanifny</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">chandrijaya</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">wisnualfiansyah97</a></li>
+                </ul>
+            </div><!-- footer-section -->
+        </div><!-- col-lg-4 col-md-6 -->
+
+        <!-- <div class="col-lg-4 col-md-6">
+            <div class="footer-section">
+
+                <h4 class="title"><b>SUBSCRIBE</b></h4>
+                <div class="input-area">
+                    <form>
+                        <input class="email-input" type="text" placeholder="Enter your email">
+                        <button class="submit-btn" type="submit"><i class="icon ion-ios-email-outline"></i></button>
+                    </form>
+                </div>
+
+            </div><!-- footer-section -->
+        </div><!-- col-lg-4 col-md-6 --> -->
 
     </div><!-- row -->
 </div><!-- container -->
